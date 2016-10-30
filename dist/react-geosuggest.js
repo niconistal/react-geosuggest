eb12ec17a5542d1982ecc0b5069251715fc3c420
Nicolas Nistal <nicolas.nistal@thegoneapp.com>
2016-10-29 21:07:21 -0300

    chore(release): version 6.0.0

--- dist/react-geosuggest.js
+++ dist/react-geosuggest.js
@@ -32125,6 +32125,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = (window.React);
@@ -32598,35 +32600,19 @@ var Geosuggest = function (_React$Component) {
         text: 'label',
         value: 'placeId'
       },
-          _props = this.props,
-          style = _props.style,
-          floatingLabelText = _props.floatingLabelText,
-          errorText = _props.errorText,
-          hintText = _props.hintText,
-          menuStyle = _props.menuStyle,
-          listStyle = _props.listStyle,
-          fullWidth = _props.fullWidth,
           filter = function filter() {
         return true;
       };
-
-      return _react2.default.createElement(_AutoComplete2.default, {
+      return _react2.default.createElement(_AutoComplete2.default, _extends({
         dataSource: this.state.suggests,
-        style: style,
-        floatingLabelText: floatingLabelText,
-        errorText: errorText,
-        hintText: hintText,
-        menuStyle: menuStyle,
-        listStyle: listStyle,
         dataSourceConfig: suggestsConfig,
-        onUpdateInput: this.onInputChange,
         filter: filter,
         onFocus: this.onInputFocus,
         value: this.state.userInput,
-        fullWidth: fullWidth,
-        openOnFocus: true,
+        openOnFocus: true
+      }, this.props, {
         onNewRequest: this.selectSuggest
-      });
+      }));
     }
   }]);
 
